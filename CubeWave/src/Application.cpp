e6da43eb7a216782d57a23c6800167a1849af1bb
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-08-25 17:53:20 +0200

    Created C++ project

---
+++ CubeWave/src/Application.cpp
@@ -0,0 +1,141 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <string>
+
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	// Initialize the library
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a windowed mode window and its OpenGL context
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Error initializing GLEW" << std::endl;
+		return 0;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[] = {
+		300.f, 300.f, 0.0f, 0.0f,
+		500.f, 300.f, 1.0f, 0.0f,
+		500.f, 500.f, 1.0f, 1.0f,
+		300.f, 500.f, 0.0f, 1.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	// Enable blending
+	GLCall(glEnable(GL_BLEND));
+	GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+	VertexArray* va = new VertexArray();
+	VertexBuffer* vb = new VertexBuffer(positions, 4 * 4 * sizeof(float));
+	VertexBufferLayout* layout = new VertexBufferLayout();
+	layout->Push<float>(2);
+	layout->Push<float>(2);
+	va->AddBuffer(*vb, *layout);
+	
+	IndexBuffer* ib = new IndexBuffer(indices, 6);
+
+	// Specify the bounds of the window to set the correct aspect ratio (4:3)
+	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+
+	glm::mat4 mvp = proj * view * model;
+
+	Shader* shader = new Shader("res/shaders/Basic.shader");
+	shader->Bind();
+	shader->SetUniformMat4f("u_MVP", mvp);
+
+	Texture* texture = new Texture("res/textures/Tree.png");
+	texture->Bind();
+	shader->SetUniform1i("u_Texture", 0);
+
+	va->Unbind();
+	vb->Unbind();
+	ib->Unbind();
+	shader->Unbind();
+
+	Renderer* renderer = new Renderer();
+
+	float r = 0.0f;
+	float increment = 0.05f;
+	// Loop until the user closes the window
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		renderer->Clear();
+
+		shader->Bind();
+		shader->SetUniform4f("u_Color", r, .2f, .5f, 1.0f);
+
+		renderer->Draw(*va, *ib, *shader);
+
+		if (r > 1.0f)
+		{
+			increment = -.05f;
+		}
+		else if (r < 0.0f)
+		{
+			increment = .05f;
+		}
+
+		r += increment;		
+
+		// Swap front and back buffers
+		glfwSwapBuffers(window);
+
+		// Poll for and process events
+		glfwPollEvents();
+	}
+
+	delete vb;
+	delete ib;
+	delete layout;
+	delete shader;
+	delete texture;
+	delete renderer;
+
+	glfwTerminate();
+	return 0;
+}
